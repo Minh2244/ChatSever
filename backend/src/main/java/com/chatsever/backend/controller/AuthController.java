0404bd5b53bb02f659ceaed995440ec774be4211
Minh2244 <minhmap3367@gmail.com>
2026-03-23 14:41:02 +0700

    Enable CORS for all controllers

--- backend/src/main/java/com/chatsever/backend/controller/AuthController.java
+++ backend/src/main/java/com/chatsever/backend/controller/AuthController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequiredArgsConstructor
 public class AuthController {
 
--- backend/src/main/java/com/chatsever/backend/controller/FriendshipController.java
+++ backend/src/main/java/com/chatsever/backend/controller/FriendshipController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/friendships")
 @RequiredArgsConstructor
 public class FriendshipController {
